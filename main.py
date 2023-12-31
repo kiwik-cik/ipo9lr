35261d3bbeb6b35c6a34fd3c4fa5cfee1f17e8a7
kiwik-cik <145658906+kiwik-cik@users.noreply.github.com>
2023-12-31 16:26:23 +0300

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 def armstrong_numbers(k):
-    armstrong_nums = []
+    armstrong_nums = []  # Создаем пустой список для хранения чисел Армстронга
 
-    for num in range(1, k + 1):
-        num_str = str(num)
-        n = len(num_str)
-        sum_of_digits = sum(int(digit)**n for digit in num_str)
+    for num in range(1, k + 1):  # Итерируем через все числа от 1 до k
+        num_str = str(num)  # Преобразуем число в строку
+        n = len(num_str)  # Находим количество цифр в числе
+        sum_of_digits = sum(int(digit)**n for digit in num_str)  # Вычисляем сумму цифр числа, возведенных в степень количества цифр
 
-        if sum_of_digits == num:
-            armstrong_nums.append(num)
+        if sum_of_digits == num:  # Проверяем, является ли число числом Армстронга
+            armstrong_nums.append(num)  # Если да, добавляем его в список
 
-    return armstrong_nums
+    return armstrong_nums  # Возвращаем список чисел Армстронга
 k = 1000
 result = armstrong_numbers(k)
-print(result)
+print(result)  # Выводим результат
