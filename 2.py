e1776744cbb931e0b3c54928f55bde7af6fc2c33
kiwik-cik <145658906+kiwik-cik@users.noreply.github.com>
2023-12-31 16:31:48 +0300

    2.py

--- 2.py
+++ 2.py
@@ -3,21 +3,21 @@ def circle_area(r):
     return math.pi * r ** 2
 def triangle_area(b, h):
     return 0.5 * b * h
-def armstrong_numbers(k):
+def armstrong_numbers(k):# Создаем пустой список для хранения чисел Армстронга
     armstrong_nums = []
 
-    for num in range(1, k + 1):
-        num_str = str(num)
-        n = len(num_str)
-        sum_of_digits = sum(int(digit)**n for digit in num_str)
+    for num in range(1, k + 1):# Итерируем через все числа от 1 до k
+        num_str = str(num)# Преобразуем число в строку
+        n = len(num_str)# Находим количество цифр в числе
+        sum_of_digits = sum(int(digit)**n for digit in num_str)# Вычисляем сумму цифр числа, возведенных в степень количества цифр
 
-        if sum_of_digits == num:
-            armstrong_nums.append(num)
+        if sum_of_digits == num:# Проверяем, является ли число числом Армстронга
+            armstrong_nums.append(num)# Если да, добавляем его в список
 
-    return armstrong_nums
+    return armstrong_nums# Возвращаем список чисел Армстронга
 import sys
 sys.path
 import custom_functions
 k = 1000
 result = custom_functions.armstrong_numbers(k)
-print(result)
+print(result)# Выводим результат
